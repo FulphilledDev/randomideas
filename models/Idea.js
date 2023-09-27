780efff6d81108a868ac38e214efbef8e545b813
Philip Simpson <dev.phits@gmail.com>
2023-09-27 07:35:17 -0700

    Idea Model & Schema

---
+++ models/Idea.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const IdeaSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, 'Please add text content'],
+    },
+    tag: {
+      type: String,
+    },
+    username: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: new Date(),
+    },
+  }
+  //   {
+  //     timestamps: true,
+  //   }
+);
